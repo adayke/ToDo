32fe3274427fc6c33b56264e755cdbb0aa61d3e8
Nitro <nitro_1@gmail.com>
2022-02-04 23:30:08 +0600

    release v3.0

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
-// Хранилищем будет объект, а имена задач - ключами.
-// const list = [
-// { name: 'create a post', status: 'In progress', priority: 'low' }, 
-// { name: 'test', status: 'Done', priority: 'high' }] 
-
-// Функция changeStatus - будет менять статус задачи
-// changeStatus("write a post", "Done")
-
-// Функция addTask - добавляет новую задачу
-// addTask('have a walk')
-
-// Функция deleteTask - удаляет задачу
-// deleteTask('have a walk')
-
-// Функция showList будет выводить весь список дел в виде
-// Todo:
-// "create a new practice task",
-//   "make a bed",
-//   In Progress:
-// "write a post"
-// Done:
-// -
 const list = [
-  { name:"create a new practice task", status: "To Do", priority: 'Low' },
-  { name: "make a bed", status: "To Do", priority: 'Low' },
-  { name: "write a post", status: "Done", priority: "High" },  
+  { name: "create a new practice task", status: "To Do", priority: 'Low' },
+  { name: "make a bed", status: "In Progress", priority: 'Low' },
+  { name: "write a post", status: "Done", priority: "High" },
+  { name: "do sport", status: "Done", priority: "High" },  
 ];
 
 function addTask(name) {
@@ -37,7 +16,7 @@ function deleteTask(name) {
     } 
 };
 
-function changeStatus(name, status, priority) {
+function changeStatus(name, status) {
   let findName = list.find(item => item.name == name)
     findName['status'] = status
       if(status == 'Done') {
@@ -48,31 +27,57 @@ function changeStatus(name, status, priority) {
 }
   
   function showList() {
-    let noTaskStatus = true;
-    
     console.log("To Do:")
-  for (let key in list) {
-    if (list[key] == 'To Do') {
-      console.log(`\t${key}`)
-      noTaskStatus = false;
+    let findTodo = list.filter(item => item.status == "To Do")
+    if(findTodo === undefined) {
+      console.log(`\t -`);
+    } else {
+      for (let key in findTodo) {
+        console.log(findTodo[key])
+      }
     }
-  } if (noTaskStatus == true) { console.log(`\t -`) };
 
     console.log("In Progress:")
-    noTaskStatus = true;
-    for (let key in list) {
-      if (list[key] == "In Progress") {
-      console.log(`\t${key}`)
-        noTaskStatus = false;
-    }
-  } if (noTaskStatus == true) { console.log(`\t -`) }; 
+    let findInProgress = list.filter(item => item.status == "In Progress")
+    if(findInProgress === undefined) {
+      console.log(`\t -`);
+    } else {
+      for (let key in findInProgress) {
+        console.log(findInProgress[key])
+      } 
+  };  
     
   console.log("Done:")
-    noTaskStatus = true;
-    for (let key in list) {
-      if (list[key] == "Done") {
-      console.log(`\t${key}`)
-        noTaskStatus = false;
-      } 
-    } if (noTaskStatus == true) { console.log(`\t -`) }; 
+    let findDone = list.filter(item => item.status == "Done")
+    if(findDone === undefined){
+      console.log(`\t -`)
+    } else {
+      for (let key in findDone) {
+        console.log(findDone[key])
+      }
+    } 
 } showList();
+
+function showBy(prop) {
+  if(prop == 'High') {
+  let findHighPrioritys = list.filter(item => item.priority == "High")
+  if (findHighPrioritys === undefined) {
+    console.log(`\t -`)
+  } else {
+    for (let key in findHighPrioritys) {
+      console.log(findHighPrioritys[key])
+      }
+    }
+  }
+
+  if (prop == 'Low') {
+    let findLowPrioritys = list.filter(item => item.priority == "Low")
+    if (findLowPrioritys === undefined) {
+      console.log(`\t -`)
+    } else {
+      for (let key in findLowPrioritys) {
+        console.log(findLowPrioritys[key])
+      }
+    }
+  }
+}
