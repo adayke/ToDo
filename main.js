250aaa39d98d7c3f57717863e9d10a75a829eefd
Nitro <nitro_1@gmail.com>
2022-02-04 14:16:44 +0600

    release v5.0

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+// Хранилищем будет объект, а имена задач - ключами.
+// const list = [
+// { name: 'create a post', status: 'In progress', priority: 'low' }, 
+// { name: 'test', status: 'Done', priority: 'high' }] 
+
+// Функция changeStatus - будет менять статус задачи
+// changeStatus("write a post", "Done")
+
+// Функция addTask - добавляет новую задачу
+// addTask('have a walk')
+
+// Функция deleteTask - удаляет задачу
+// deleteTask('have a walk')
+
+// Функция showList будет выводить весь список дел в виде
+// Todo:
+// "create a new practice task",
+//   "make a bed",
+//   In Progress:
+// "write a post"
+// Done:
+// -
 const list = [
   {name:"create a new practice task", status: "To Do", priority: 'Low' },
   {name: "make a bed", status: "To Do", priority: 'Low' },
