63f8dcbb9d9662c786c2fd57d580f6a4da0293f9
Nitro <nitro_1@gmail.com>
2022-02-04 14:18:51 +0600

    release v5.0

--- main.js
+++ main.js
@@ -21,30 +21,30 @@
 // Done:
 // -
 const list = [
-  {name:"create a new practice task", status: "To Do", priority: 'Low' },
-  {name: "make a bed", status: "To Do", priority: 'Low' },
-  {name: "write a post", status: "Done", priority: "High" },  
+  { name:"create a new practice task", status: "To Do", priority: 'Low' },
+  { name: "make a bed", status: "To Do", priority: 'Low' },
+  { name: "write a post", status: "Done", priority: "High" },  
 ];
 
 function addTask(name) {
-list.push({ name: name, status: 'To Do', priority: 'Low' })
+  list.push({ name: name, status: 'To Do', priority: 'Low' })
 };
 
 function deleteTask(name) {
-let findIndex = list.findIndex(item => item.name == name)
-if(findIndex !== -1) {
-  list.splice(findIndex, 1)
-  } 
+  let findIndex = list.findIndex(item => item.name == name)
+    if(findIndex !== -1) {
+      list.splice(findIndex, 1)
+    } 
 };
 
 function changeStatus(name, status, priority) {
-let findName = list.find(item => item.name == name)
-findName['status'] = status
-if(status == 'Done') {
-findName['priority'] = 'High'
-} else {
-findName['priority'] = 'Low'
-}
+  let findName = list.find(item => item.name == name)
+    findName['status'] = status
+      if(status == 'Done') {
+        findName['priority'] = 'High'
+      } else {
+        findName['priority'] = 'Low'
+      }
 }
   
   function showList() {
